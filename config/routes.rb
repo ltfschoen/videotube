b8fe0e0a361fd28d8976792bc5fa3e674a83157d
Luke Schoen <ltfschoen@gmail.com>
2017-09-03 08:50:35 +1000

    fix: Fix routes so tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
   
-  get '/pages/index', to: 'patients#index'
+  get '/pages/index', to: 'pages#index'
 
-  get '/pages/about', to: 'patients#about'
+  get '/pages/about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
